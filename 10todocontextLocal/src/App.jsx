255b22683930f6585aafd6e61559cae7d139b234
Amaara96 <amara.alex96@gmail.com>
2026-01-22 03:13:42 +0100

    toggle

---
+++ 10todocontextLocal/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { TodoProvider } from './contexts'
+
+function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id: Date.now(), ...todo} , ...prev])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === todo.id ? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => 
+      prev.map((prevTodo) => 
+        prevTodo.id === id ? {...prevTodo, completed:
+        !prevTodo.completed} : prevTodo
+      )
+    )
+  }
+
+  return (
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+      <h1 className='text-3xl font-bold underline'>Hello world</h1>
+    </TodoProvider>
+  )
+}
+
+export default App
---
+++ 10todocontextLocal/src/contexts/index.js
@@ -0,0 +1,1 @@
+export {TodoContext, TodoProvider, useTodob } from "./TodoContext"
