924168bea51c53d2adf3dd7a0d369dc7d78d8856
Amaara96 <amara.alex96@gmail.com>
2026-01-22 05:03:22 +0100

    todo

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -28,9 +28,24 @@ function App() {
     )
   }
 
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if (todos && todos.length > 0){
+    setTodos(todos) 
+    }
+
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
-      <h1 className='text-3xl font-bold underline'>Hello world</h1>
+      <TodoForm/>
+      {
+        
+      }
     </TodoProvider>
   )
 }
